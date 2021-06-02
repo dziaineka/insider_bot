f7fd03ee377303cfedfd2cbc954c27b568b2a608
Andrei Dziaineka <a_dziaineka@wargaming.net>
2021-06-02 11:29:27 +0300

    add default values

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from os import getenv
 
-BOT_TOKEN = getenv('BOT_TOKEN')
-CHAT_NAME = getenv('CHAT_NAME')
-INSIDE_CHANNEL = getenv('INSIDE_CHANNEL')
+BOT_TOKEN = getenv('BOT_TOKEN', "")
+CHAT_NAME = getenv('CHAT_NAME', "")
+INSIDE_CHANNEL = getenv('INSIDE_CHANNEL', "")
