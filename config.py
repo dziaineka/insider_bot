6a7e3a2c59af79fdfefa2a04ff7e3fb5926f66ae
Andrei Dziaineka <a_dziaineka@wargaming.net>
2021-06-02 11:35:47 +0300

    add .env support

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 from os import getenv
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# Create .env file path.
+dotenv_path = join(dirname(__file__), ".env")
+
+# Load file from the path.
+load_dotenv(dotenv_path)
 
 BOT_TOKEN = getenv('BOT_TOKEN', "")
 CHAT_NAME = getenv('CHAT_NAME', "")
